b83b9bade95047da99bae881e841adefff915938
Kunal <kunalmanu2468@gmail.com>
2025-03-06 20:18:18 +0530

    refactoring code

--- src/app-mry/layout/Service.vue
+++ src/app-mry/layout/Service.vue
@@ -3,13 +3,38 @@
     <!-- Header Section -->
     <h1 class="text-2xl sm:text-3xl mx-4 font-bold mb-2">Solutions</h1>
     <p class="text-gray-600 mb-4 mx-4 max-w-2xl text-sm sm:text-md">
-      At our digital marketing agency, we offer a range of services to help businesses grow and succeed online. These services include:
+      At our digital marketing agency, we offer a range of services to help businesses grow and succeed online. These
+      services include:
     </p>
+
 
     <!-- Services Grid -->
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mx-4 gap-4 mb-2">
-      <!-- Customer Management -->
-      <div 
+      <ArticleList :contentType="'services'" v-slot="{ loading, results }">
+        <template v-if="loading">
+          <div>Loading testimonials...</div>
+        </template>
+        <template v-else>
+          <div v-for="(item, index) in results" :key="index" @click="handleNavigation(item.info.hyperlink)"
+            class="rounded-2xl sm:w-full p-6 transition-all hover:scale-[1.02] hover:shadow-lg cursor-pointer h-52"
+            :style="{
+              backgroundColor: item.info.divcolor,
+              borderBottom: `4px solid ${item.info.bordercolor}`
+            }">
+            <div class="h-28 relative">
+              <h2 class="text-xl sm:text-2xl font-bold">{{ item.info.title }}</h2>
+              <img :src="item.info.image" alt="Service Image"
+                class="hidden sm:block absolute right-0 top-0 w-32 h-28" />
+            </div>
+            <button class="inline-flex items-center gap-1" :style="{ color: item.info.textcolor }">
+              <img src="../../@assets/images/Icon.svg" class="w-6 sm:w-8 h-6 sm:h-8" />
+              <span class="text-sm sm:text-lg">Learn more</span>
+            </button>
+          </div>
+        </template>
+      </ArticleList>
+
+      <!-- <div 
         @click="handleCustomer"
         class="rounded-2xl bg-blue-50 sm:w-full p-6 text-gray-800 transition-all hover:scale-[1.02] hover:shadow-lg border-b-4 border-blue-200 cursor-pointer h-52"
       >
@@ -24,10 +49,10 @@
           <img src="../../@assets/images/Icon.svg" class="w-6 sm:w-8 h-6 sm:h-8" />
           <span class="text-sm sm:text-lg">Learn more</span>
         </button>
-      </div>
+      </div> -->
 
       <!-- Lead Management -->
-      <div 
+      <!-- <div 
         @click="handleLead"
         class="rounded-2xl bg-purple-50 p-6 text-gray-800 transition-all hover:scale-[1.02] hover:shadow-lg border-b-4 border-purple-200 cursor-pointer h-52"
       >
@@ -42,10 +67,10 @@
           <img src="../../@assets/images/Icon(1).svg" class="w-6 sm:w-8 h-6 sm:h-8" />
           <span class="text-sm sm:text-lg">Learn more</span>
         </button>
-      </div>
+      </div> -->
 
       <!-- Relationship Management -->
-      <div 
+      <!-- <div 
         @click="handleRelation"
         class="rounded-2xl bg-teal-50 p-6 text-gray-800 transition-all hover:scale-[1.02] hover:shadow-lg border-b-4 border-teal-200 cursor-pointer h-52"
       >
@@ -60,10 +85,10 @@
           <img src="../../@assets/images/Icon(1).svg" class="w-6 sm:w-8 h-6 sm:h-8" />
           <span class="text-sm sm:text-lg">Learn more</span>
         </button>
-      </div>
+      </div> -->
 
       <!-- Conversation Management -->
-      <div 
+      <!-- <div 
         @click="handleConvo"
         class="rounded-2xl bg-indigo-50 p-6 text-gray-800 transition-all hover:scale-[1.02] hover:shadow-lg border-b-4 border-indigo-200 cursor-pointer h-52"
       >
@@ -78,10 +103,10 @@
           <img src="../../@assets/images/Icon.svg" class="w-6 sm:w-8 h-6 sm:h-8" />
           <span class="text-sm sm:text-lg">Learn more</span>
         </button>
-      </div>
+      </div> -->
 
       <!-- API Services -->
-      <div 
+      <!-- <div 
         @click="handleApi"
         class="rounded-2xl bg-rose-50 p-6 text-gray-800 transition-all hover:scale-[1.02] hover:shadow-lg border-b-4 border-rose-200 cursor-pointer h-52"
       >
@@ -96,57 +121,49 @@
           <img src="../../@assets/images/Icon.svg" class="w-6 sm:w-8 h-6 sm:h-8" />
           <span class="text-sm sm:text-lg">Learn more</span>
         </button>
-      </div>
+      </div> -->
 
       <!-- Campaign Management -->
-      <div 
-        @click="handleCampaign"
-        class="rounded-2xl bg-amber-50 p-6 text-gray-800 transition-all hover:scale-[1.02] hover:shadow-lg border-b-4 border-amber-200 cursor-pointer h-52"
-      >
+      <!-- <div @click="handleCampaign"
+        class="rounded-2xl bg-amber-50 p-6 text-gray-800 transition-all hover:scale-[1.02] hover:shadow-lg border-b-4 border-amber-200 cursor-pointer h-52">
         <div class="h-28 relative">
           <h2 class="text-xl sm:text-2xl font-bold">Campaign</h2>
           <h2 class="text-xl sm:text-2xl font-bold">Management</h2>
-          <img src="../../@assets/images/tokyo-volumetric-analytics-of-different-types-in-web-browsers 2.svg" 
-               alt="" 
-               class="hidden sm:block absolute right-0 top-0 w-32 h-28" />
+          <img src="../../@assets/images/tokyo-volumetric-analytics-of-different-types-in-web-browsers 2.svg" alt=""
+            class="hidden sm:block absolute right-0 top-0 w-32 h-28" />
         </div>
         <button class="inline-flex items-center text-amber-600 hover:text-amber-700 gap-1">
           <img src="../../@assets/images/Icon(1).svg" class="w-6 sm:w-8 h-6 sm:h-8" />
           <span class="text-sm sm:text-lg">Learn more</span>
         </button>
-      </div>
+      </div> -->
     </div>
 
     <!-- BOTs Section -->
-    <div 
-      @click="handleBot"
-      class="rounded-2xl bg-emerald-50 px-6 py-3 text-gray-800 mx-4 transition-all hover:scale-[1.02] hover:shadow-lg border-b-4 border-emerald-200 cursor-pointer relative flex flex-col sm:flex-row justify-between items-center sm:items-start"
-    >
+    <div @click="handleBot"
+      class="rounded-2xl bg-emerald-50 px-6 py-3 text-gray-800 mx-4 transition-all hover:scale-[1.02] hover:shadow-lg border-b-4 border-emerald-200 cursor-pointer relative flex flex-col sm:flex-row justify-between items-center sm:items-start">
       <div class="max-w-md mt-3">
         <h2 class="text-xl font-bold mb-3">BOTs & BOT Services</h2>
         <p class="mb-4 text-sm">
-          Our advanced BOTs efficiently manage up to 80% of repetitive inquiries, allowing your team to focus on high-value, personalized interactions that drive deeper customer relationships.
+          Our advanced BOTs efficiently manage up to 80% of repetitive inquiries, allowing your team to focus on
+          high-value, personalized interactions that drive deeper customer relationships.
         </p>
       </div>
-      <img 
-        src="../../@assets/images/botService.png" 
-        alt="" 
-        class="hidden sm:block w-44 h-32"
-      />
+      <img src="../../@assets/images/botService.png" alt="" class="hidden sm:block w-44 h-32" />
     </div>
   </div>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
+import ArticleList from "@components/ArticleList.vue";
+
 const router = useRouter();
 
 // Router functions
-const handleCustomer = () => router.push('/customer');
-const handleLead = () => router.push('/lead');
-const handleRelation = () => router.push('/relation');
-const handleConvo = () => router.push('/conversation');
-const handleApi = () => router.push('/api');
-const handleCampaign = () => router.push('/campaign');
-const handleBot = () => router.push('/bot');
+const handleNavigation = (path) => {
+  if (path) router.push(path);
+};
+
+
 </script>
