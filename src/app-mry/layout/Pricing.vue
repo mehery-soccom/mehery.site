3a48c071300f98e34a304e956f1f5b3a0b03c290
Kunal Chaturvedi <96841396+KCmean@users.noreply.github.com>
2025-04-09 18:23:31 +0530

    Update Pricing.vue

--- src/app-mry/layout/Pricing.vue
+++ src/app-mry/layout/Pricing.vue
@@ -262,22 +262,10 @@ import axios from "axios";
 
 import { ref, computed, onMounted } from "vue";
 import { CheckIcon, XIcon } from "vue-feather-icons";
-import resource from "@components/Resource"
 
 const showUSD = ref(false);
 const showFeatures = ref(false);
 const billingCycle = ref("Monthly");
-
-const json = await resource.read({ contentType: "pricing", src: "pricing_list.xlsx", query: {
-    sheet: "Pricing List",
-    header: 1,
-    range: "A1:Z1000",
-    valueRenderOption: "FORMULA",
-    dateTimeRenderOption: "FORMATTED_STRING"
-} });
-
-
-
 
 // const toggleCurrency = () => {
 //     showUSD.value = !showUSD.value;
