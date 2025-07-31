f3ef498dc0baee8699bb1877f6875ff4f0aec7d1
Tanmay Shidhani <tanmay.shidhani57@gmail.com>
2025-07-31 10:54:43 +0530

    webchat UI

--- src/app-mry/layout/WebChat.vue
+++ src/app-mry/layout/WebChat.vue
@@ -15,19 +15,25 @@ onMounted(() => {
     channelId: 'web:meherysitepagesdev',
     channelKey: '1joi34fachojo9OAG550I72',
     config: {
-      "launcher.animation.effect": "bounce",
-      'header.bg.color': '#78bffd',
-      'header.text.color': '#000000',
+      "chatWindow.theme.color": "#b98e33",
+
+      'header.text.color': '#ffffff',
       'header.icon.url': 'https://cdn.jsdelivr.net/gh/mehery-soccom/mehery-web-dist@834bfa2c3b8060cac2ebcd7778758d6021be2dca/dist/logo/logo-tiny-o.png',
       'header.title.text': 'Lets Chat ...',
-      'launcher.bg.color': '#4f46e5',
+
+      "launcher.autoOpen.active": "true",
+      'launcher.bg.color': '#b98e33',
+
       'messageList.bg.color': '#ffffff',
-      'sentMessage.bg.color': '#78bffd',
-      'sentMessage.text.color': '#000000',
-      'receivedMessage.bg.color': '#ffd898',
-      'receivedMessage.text.color': '#000000',
-      'userInput.bg.color': '#78bffd',
-      'userInput.text.color': '#000000'
+
+      "sentMessage.text.color" : "#ffffff",
+
+      "receivedMessage.bg.color" : "#eaeaea",
+      "receivedMessage.text.color" : "#222222",
+
+      "userInput.bg.color" : "#f4f7f9",
+      "userInput.text.color" : "#565867",
+      "userInput.button.color" : "#ffffff",
     }
   })
   document.body.appendChild(webchatScript)
